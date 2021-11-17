b30a860d5571495124d0f4c6ca9d957a618cae19
Rob Mongin <RobertMongin@outlook.com>
2021-11-16 20:36:03 -0500

    fixed files

---
+++ CompanyOutingRepo_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CompanyOutingRepo_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CompanyOutingRepo_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("26a2712c-b67b-4508-8333-e033538149ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- CompanyOutingsRepository/OutingsRepo.cs
+++ CompanyOutingsRepository/OutingsRepo.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BadgesRepository
+namespace CompanyOutingsRepository
 {
-    public class Class1
+    class OutingsRepo
     {
     }
 }
